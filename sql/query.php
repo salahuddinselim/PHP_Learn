cbffd4269a5859720f7d8813e367ef4ef565e458
Salah Uddin <selimsalahuddin19@gmail.com>
2026-02-04 20:30:03 +0600

    added execute

--- sql/query.php
+++ sql/query.php
@@ -4,3 +4,7 @@ $sql = "create table users(
     id int auto_increment primary key,
     name varchar(50) not null,
     gpa varchar(10) not null )";
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+print "New table added in there.";
