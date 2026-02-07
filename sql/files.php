57092e4d9e836fd595913a40b8765d27a8c2e742
Salah Uddin <selimsalahuddin19@gmail.com>
2026-02-08 00:17:14 +0600

    added comment

--- sql/files.php
+++ sql/files.php
@@ -1,9 +1,19 @@
 <?php
+//check the attributes of files
 print_r($_FILES);
+
 echo "<br>";
+
+
+
 if ($_FILES['fileupload']) {
+    //check the file name, as $_FILES is a 2D array so needs to values, fileupload from the name of form and name is the given default which we can check in the 1st line
     $path = $_FILES['fileupload']['name'];
+
+    //creating uploading path by using different folder and the path name of the file
     $upload_path = "./uploads/" . $path;
+
+    //move uploaded file do take 2 inputs the temporary address which changes every time and the upload path where the file will store. 
     if (move_uploaded_file($_FILES['fileupload']['tmp_name'], $upload_path)) {
         echo "file uploaded successfully";
     } else {
