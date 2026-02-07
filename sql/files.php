cb0d43719c914536307955fac8f638701b5755a7
Salah Uddin <selimsalahuddin19@gmail.com>
2026-02-08 00:05:24 +0600

    added

--- sql/files.php
+++ sql/files.php
@@ -1,7 +1,23 @@
 <?php
 print_r($_FILES);
-
+echo "<br>";
 if ($_FILES['fileupload']) {
     $path = $_FILES['fileupload']['name'];
     $upload_path = "./uploads/" . $path;
+    if (move_uploaded_file($_FILES['fileupload']['tmp_name'], $upload_path)) {
+        echo "file uploaded successfully";
+    } else {
+        die("File does not uploaded");
+    }
+    echo "<br>";
+    echo "<br>";
+    echo $_FILES['fileupload']['type'];
+    $type = $_FILES['fileupload']['type'];
+    echo "<br>";
+    echo "<br>";
+    if ($type == 'image/png' || $type == 'image/jpeg') {
+        echo "<img src='$upload_path' alt='Added new image' width = 200px height = 300>";
+    } else {
+        echo "<a href='$upload_path'>Download</a>";
+    }
 }
