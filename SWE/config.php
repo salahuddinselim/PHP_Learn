2e60ea4094be4cae1c4a90b77588bbba99e15143
Salah Uddin <selimsalahuddin19@gmail.com>
2026-04-14 21:14:06 +0600

    added

---
+++ SWE/config.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$username = "root";
+$dbname = "Cricket";
+$password = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
