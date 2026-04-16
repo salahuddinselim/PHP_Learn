6a5332fc747f79d480312c3533671b43214a8064
Salah Uddin <selimsalahuddin19@gmail.com>
2026-04-16 23:21:08 +0600

    added

--- SWE_MID/config.php
+++ SWE_MID/config.php
@@ -3,3 +3,15 @@ $host = "localhost";
 $dbname = "tournament";
 $username = "root";
 $password = "";
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname",
+        $username,
+        $password
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Database connected successfully!</br>";
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
